da7917c8ad21820f16bce12a1c3298777b247734
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-11-08 14:28:07 +0300

    post

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -1,9 +1,12 @@
 from flask import render_template
 from app.posts import bp
+from app.extensions import db
+from app.models.post import Post
 
 @bp.route('/')
 def index():
-    return render_template('posts/index.html')
+    posts = Post.query.all()
+    return render_template('posts/index.html', posts=posts)
 
 @bp.route('/categories/')
 def categories():
